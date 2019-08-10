71e0357b48eab2de3eaa2e732af29cf3f1ffd5d0
Abagail Hudson <abagailanne@gmail.com>
2019-08-10 20:35:29 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,7 +35,7 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id/edit' do 
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     @owners = Owner.all
     erb :'/pets/edit'
